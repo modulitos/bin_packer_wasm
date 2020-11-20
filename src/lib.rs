aa460a61ac5e050bcd553e4e3c6f1d300f61b2c6
modulitos <modulitos@users.noreply.github.com>
2020-11-19 23:45:58 -0800

    basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use bin_packer_3d::bin::Bin;
+use bin_packer_3d::item::Item;
+use bin_packer_3d::packing_algorithm::packing_algorithm;
+use crate::utils::set_panic_hook;
+
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )*).into());
+    }
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn setup() {
+    set_panic_hook();
+    log!("setup complete!");
+}
 
 #[wasm_bindgen]
 extern {
@@ -17,3 +36,23 @@ extern {
 pub fn greet() {
     alert("Hello, wasm-previewer!");
 }
+
+#[wasm_bindgen]
+pub struct BinPacker {}
+
+#[wasm_bindgen]
+impl BinPacker {
+    pub fn new() -> Self {
+        log!("creating the bin packer!");
+
+        let deck = Item::new("deck", [2, 8, 12]);
+        let die = Item::new("die", [8, 8, 8]);
+        let items = vec![deck, deck, die, deck, deck];
+        log!("items: {:?}", items);
+
+        let _packed_items = packing_algorithm(Bin::new([8, 8, 12]), &items);
+        Self {}
+    }
+}
+
+
--- tests/web.rs
+++ tests/web.rs
@@ -4,10 +4,17 @@
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
+use wasm_previewer::BinPacker;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
     assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test]
+pub fn test_bin_pack() {
+    let _bp = BinPacker::new();
+    assert_eq!(1, 1);
 }
