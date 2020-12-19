775478f5d99012808f50ed0842253d131591b91f
modulitos <modulitos@users.noreply.github.com>
2020-12-18 23:19:46 -0800

    use custom css styles

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,9 +1,22 @@
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      teal: colors.teal,
+      white: colors.white,
+    },
   },
   variants: {
     extend: {},
@@ -11,5 +24,5 @@ module.exports = {
   plugins: [],
   corePlugins: {
     preflight: true,
-  }
-}
+  },
+};
